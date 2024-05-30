cd89396b4d3a5536a2716f5e26ef62fe17adc8b0
kjw0384 <kjw0384@gmail.com>
2024-05-30 12:07:08 +0900

    case7 green

--- Sample-Test1/prime-factors_test.cpp
+++ Sample-Test1/prime-factors_test.cpp
@@ -38,3 +38,8 @@ TEST_F(PrimeFixture, Of6) {
 	expected = { 3, 3 };
 	EXPECT_EQ(expected, prime_factor.of(9));
 }
+
+TEST_F(PrimeFixture, Of7) {
+	expected = { 2, 2, 3 };
+	EXPECT_EQ(expected, prime_factor.of(12));
+}
