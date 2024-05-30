be5ffd684ced8c5aec4657a10d1f2f4842dec552
kjw0384 <kjw0384@gmail.com>
2024-05-30 11:49:52 +0900

    case2 green

--- Sample-Test1/prime-factors_test.cpp
+++ Sample-Test1/prime-factors_test.cpp
@@ -8,3 +8,9 @@ TEST(PrimeFacgors, Of1) {
 	vector<int> expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1));
 }
+
+TEST(PrimeFacgors, Of2) {
+	PrimeFactor prime_factor;
+	vector<int> expected = {2};
+	EXPECT_EQ(expected, prime_factor.of(2));
+}
